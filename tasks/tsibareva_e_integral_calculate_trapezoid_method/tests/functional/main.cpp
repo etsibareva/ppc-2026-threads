1c05afc51d5efde1f61dcea5a05955b53e1602d2
etsibareva <etsibareva.university@gmail.com>
2026-03-17 20:55:59 +0300

    omp for advancement

--- tasks/tsibareva_e_integral_calculate_trapezoid_method/tests/functional/main.cpp
+++ tasks/tsibareva_e_integral_calculate_trapezoid_method/tests/functional/main.cpp
@@ -49,13 +49,12 @@ TEST_P(TsibarevaERunFuncTestsThreads, IntegralCalculation) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 7> kTestParams = {
+const std::array<TestType, 6> kTestParams = {
     std::make_tuple(IntegralTestType::kSuccessSimple2D, "2d_simple"),
     std::make_tuple(IntegralTestType::kSuccessConstant2D, "2d_constant"),
     std::make_tuple(IntegralTestType::kSuccessSimple3D, "3d_simple"),
     std::make_tuple(IntegralTestType::kSuccessConstant3D, "3d_constant"),
     std::make_tuple(IntegralTestType::kInvalidLowerBoundEqual, "invalid_lower_bound_equal"),
-    std::make_tuple(IntegralTestType::kInvalidStepsNegative, "invalid_steps_negative"),
     std::make_tuple(IntegralTestType::kInvalidEmptyBounds, "invalid_empty_bounds")};
 
 const auto kTestTasksList =
