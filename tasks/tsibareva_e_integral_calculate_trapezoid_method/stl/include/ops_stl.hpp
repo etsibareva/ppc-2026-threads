e9d353e2834ad720da8449c1e5a93f12aa416eca
etsibareva <etsibareva.university@gmail.com>
2026-05-01 12:52:51 +0300

    perf optimize

--- tasks/tsibareva_e_integral_calculate_trapezoid_method/stl/include/ops_stl.hpp
+++ tasks/tsibareva_e_integral_calculate_trapezoid_method/stl/include/ops_stl.hpp
@@ -19,9 +19,9 @@ class TsibarevaEIntegralCalculateTrapezoidMethodSTL : public BaseTask {
   bool PreProcessingImpl() override;
   bool RunImpl() override;
   bool PostProcessingImpl() override;
-  void MWork(int thread_id, int start, int end, const std::vector<int>& sizes, const std::vector<double>& h,
-           std::vector<double>& partial_sums, int dim, const std::vector<int>& steps, const std::vector<double>& lo,
-           const std::function<double(const std::vector<double>&)>& f);
+  void MWork(int thread_id, int start, int end, const std::vector<int> &sizes, const std::vector<double> &h,
+             std::vector<double> &partial_sums, int dim, const std::vector<int> &steps, const std::vector<double> &lo,
+             const std::function<double(const std::vector<double> &)> &f);
 };
 
 }  // namespace tsibareva_e_integral_calculate_trapezoid_method
